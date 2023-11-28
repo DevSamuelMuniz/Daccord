73163e29b80ffafa83df7c3eb257bdb089044d98
DevSamuelMuniz <samuel@dev.com>
2023-11-28 19:24:28 -0300

    sub modulos

---
+++ app/src/main/java/com/example/ichords/modulo_um.kt
@@ -0,0 +1,11 @@
+package com.example.ichords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class modulo_um : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modulo_um)
+    }
+}
--- app/src/main/java/com/example/ichords/trilha.kt
+++ app/src/main/java/com/example/ichords/trilha.kt
@@ -1,16 +1,25 @@
 package com.example.ichords
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class trilha : AppCompatActivity() {
+    private lateinit var modulo1: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.trilha)
 
+        modulo1 = findViewById(R.id.modulo1)
 
+        modulo1.setOnClickListener {
+            val intent = Intent(this, modulo_um::class.java)
+            startActivity(intent)
+
+        }
     }
     override fun onBackPressed() {
         super.onBackPressed()
