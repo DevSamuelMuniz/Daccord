fbebda35b7fc60cb29e59833b93d6da841a0febe
DevSamuelMuniz <samuel@dev.com>
2023-11-29 00:16:16 -0300

    fiz fav_btn.xml

--- app/src/main/java/com/example/ichords/modulo_um.kt
+++ app/src/main/java/com/example/ichords/modulo_um.kt
@@ -3,16 +3,25 @@ package com.example.ichords
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 
 class modulo_um : AppCompatActivity() {
     private lateinit var voltar: ImageButton
+    private lateinit var aula: Layout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modulo_um)
 
         voltar = findViewById(R.id.boltarBtn)
+
+
+        val includedLayout = findViewById<View>(R.id.ctnModulo)
+        includedLayout.setOnClickListener {
+
+        }
 
         voltar.setOnClickListener {
             val intent = Intent(this, trilha::class.java)
@@ -24,5 +33,4 @@ class modulo_um : AppCompatActivity() {
         super.onBackPressed()
         overridePendingTransition(0, 0)
     }
-
 }
