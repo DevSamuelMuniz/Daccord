dbc7f9b0c35f282335ffc5ea628c68905aeac97f
DevSamuelMuniz <samuel@dev.com>
2023-12-06 02:52:18 -0300

    adicionei os videos, e os layouts dos videos

---
+++ app/src/main/java/com/example/ichords/Concluido.kt
@@ -0,0 +1,20 @@
+package com.example.tela2
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ichords.R
+
+class Concluido : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_concluido)
+    }
+
+    fun onClickAvancar(view: View) {
+
+    }
+
+}
+
